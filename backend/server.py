e0135752d9aa148c5f632d5e8976dc69d2547c16
kindofwater <bedso@snu.ac.kr>
2025-07-21 23:26:05 +0900

    app.route

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+#https://youtu.be/03r7sloAyOY?si=Ztc2Vl3CZQnn_iks
+
+#input 종류마다 app.route하여 경로 설정
+#input 종류 확실히 하고 그것만 react에서 받아오면 될듯
+
+#cloud 어디에 올릴지 고민해봐야할듯
 @app.route('/api/select', methods=['POST'])
 def select():
     data = request.json
