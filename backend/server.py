656f3172da1eed986fe37263c9f1d81231ee7880
kindofwater <bedso@snu.ac.kr>
2026-04-13 14:53:10 +0900

    change

--- backend/server.py
+++ backend/server.py
@@ -4,17 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-#https://youtu.be/03r7sloAyOY?si=Ztc2Vl3CZQnn_iks
 
-#input 종류마다 app.route하여 경로 설정
-#input 종류 확실히 하고 그것만 react에서 받아오면 될듯
-
-#cloud 어디에 올릴지 고민해봐야할듯
-@app.route('/api/select', methods=['POST'])
-def select():
-    data = request.json
-    print(f"클릭한 위치: 행={data['row']}, 열={data['col']}")
-    return {'status': 'ok'}
-
-if __name__ == '__main__':
-    app.run(port=5000)
+@app.route("/game", methods = ["POST"])
+def game_start() :
+    return {}
