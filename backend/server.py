a8d903ab738fcc5111858d648e3eb9ce6cad258c
kindofwater <bedso@snu.ac.kr>
2026-04-15 17:18:51 +0900

    Add POST /game w/out moving

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
+import uuid
+import util
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
+games = {}
+Error_log = []
 
-@app.route("/game", methods = ["POST"])
-def game_start() :
-    return {}
+# @app.route("/")
+# def home():
+#     return
+
+@app.route("/game", methods=["POST"])
+def game():
+    try:
+        session_id = str(uuid.uuid4())
+        games[session_id] = {"board" : util.Board()}
+        response = make_response(jsonify({"success" : True}))
+    except:
+        session_id = str(uuid.uuid4())
+        games[session_id] = {"board" : util.Board()}
+        response = make_response(jsonify({"success" : False}))
+        print("There is *** Error in Post /game! and Logged")
+        Error_log.append({"type" : "session_id_failure"})
+    finally :
+        response.set_cookie("session_id", session_id, samesite="None")
+        return response
+
+if __name__ == "__main__" :
+    app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
+import logo from "./logo.svg"
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
@@ -14,27 +15,63 @@ function App() {
 
   if(gameState === "start"){
     return(
-      <div className = "div_all">
-        <div className="overlay">
-        <div className = "modal">
-          Divide this to Third to set all the scripts. <br></br>
-        </div></div>
-      <div className="header-box">
-        Maybe the message should change due to Server response, doesn't it? <br></br>
-      </div>
-      <div className="grid-box">
-    <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
-    <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
-    <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
-    <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
-      </div>
-    </div>
+      <StartScreen
+      onGameStart={()=>{setgameState("First_try")}}
+      showModal={showModal}
+      onCloseModal={()=>setshowModal(false)}/>
     )
     
+  }else if (gameState === "First_try"){
+    Postgame();
   }
   else{
     return(
+      <TestScreen/>
+  );
+  }
+  
+}
+
+function StartScreen({ onGameStart, showModal, onCloseModal}){
+  return (
     <div className = "div_all">
+      <div className="flexbox">
+        <div className="header-box">
+          Maybe the message should change due to Server response, doesn't it? <br></br>
+        </div>
+        <div className="game_button" onClick={onGameStart}>
+          game_start
+        </div>
+      </div>
+      <div className="grid-box">
+        <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
+        <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
+        <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
+        <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
+      </div>
+      {showModal &&(<div className="overlay" onClick={onCloseModal}>
+        <div className = "modal">
+          <div className="section">
+            <img src ={logo} alt="explain"></img>
+            <p>section 1</p>
+          </div>
+          <div className="section">
+            <img src ={logo} alt="explain"></img>
+            <p>section 2</p>
+          </div>
+          <div className="section">
+            <img src ={logo} alt="explain"></img>
+            <p>section 3</p>
+          </div>
+          
+        </div>
+        </div>)}
+    </div>
+  )
+}
+
+function TestScreen(){
+  return(<div className = "div_all">
       <div className="header-box">
         This is test page, I don't think it's supposed to pop up here.zz <br></br>
       </div>
@@ -44,10 +81,18 @@ function App() {
     <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
     <div className="circle"></div><div className="circle"></div><div className="circle"></div><div className="circle"></div>
       </div>
-    </div>
-  );
-  }
-  
+    </div>)
+}
+
+async function Postgame() {
+  const res = await fetch("http://localhost:5000/game", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+  const data = await res.json();
 }
 
 export default App;
