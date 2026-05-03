732015f94504a6667246cb8edcb2a50056c71ff2
kindofwater <bedso@snu.ac.kr>
2026-05-04 03:46:55 +0900

    zzzzㅋㅋㅋㅋ ㅋㅋㅋㅋㅋㅋ

--- backend/server.py
+++ backend/server.py
@@ -6,7 +6,7 @@ import util
 import atexit
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["https://orbitohobby.vercel.app"])
+CORS(app, supports_credentials=True, origins=["https://orbitotrial.vercel.app"])
 
 games = {}
 Error_log = []
